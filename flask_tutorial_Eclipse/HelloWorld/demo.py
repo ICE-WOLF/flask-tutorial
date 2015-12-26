72967fc8e38a49d493398e915f0cb676c44010c9
ICE-WOLF <david.dexter.charles@gmail.com>
2015-12-26 03:11:34 -0400

    mtp

--- flask_tutorial_Eclipse/HelloWorld/demo.py
+++ flask_tutorial_Eclipse/HelloWorld/demo.py
@@ -1,9 +1,11 @@
 import flask, flask.views
+import requests
 app = flask.Flask(__name__)
 
 class View(flask.views.MethodView):
     def get(self, command=None):
-        return "Hello world!"
+        #return "Hello world!"
+        return requests.get('https://api.github.com/users/ICE-WOLF').text
     
 app.add_url_rule('/', view_func=View.as_view('main'))
 
